a5b201225a1670a9e12c43b7d111f61592646254
amrdz <axmrdzc@gmail.com>
2024-11-07 12:43:01 -0600

    header

---
+++ app/Http/Controllers/ProductoController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Producto;
+use Illuminate\Http\Request;
+
+class ProductoController extends Controller
+{
+    public function index()
+    {
+        $productos = Producto::latest()->paginate(10);
+        return view('productos.index', compact('productos'));
+    }
+
+    public function create()
+    {
+        return view('productos.create');
+    }
+
+}
